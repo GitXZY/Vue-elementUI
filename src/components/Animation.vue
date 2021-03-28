d22fb3c416f151858507ca793b3767e54db2b97d
GitXzy <zhiyuan.xu1@pactera.com>
2021-03-28 22:20:50 +0800

    首次提交

---
+++ src/components/Animation.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+    <div>内置过渡动画</div>
+    <el-button type="primary" @click="return1()">返回</el-button>
+    <br>
+    <h3>淡入淡出</h3>
+    <el-button @click="show =!show">Click Me</el-button>
+    <!-- <div style="display: flex; margin-top: 20px; height: 100px;">
+      <transition name="el-fade-in-linear">
+        <div v-show="show" class="transition-box">.el-fade-in-linear</div>
+      </transition>
+      <transition name="el-fade-in">
+        <div v-show="show" class="transition-box">.el-fade-in</div>
+      </transition>
+    </div> -->
+    <div style="display:flex; justify-content: center;margin-top:20px;height:100px;">
+        <transition name="el-fade-in-linear">
+            <div v-show="show" class="transition-box">el-fade-in-linear</div>
+        </transition>
+        <transition name="el-fade-in">
+            <div v-show="show" class="transition-box">el-fade-in</div>
+        </transition>
+    </div>
+    </div>
+</template>
+<script>
+import 'element-ui/lib/theme-chalk/base.css';
+export default {
+    
+  name: "Animation",
+   data:()=>{
+       return {
+       show:true
+       }
+   },
+  methods: {
+    return1() {
+      this.$router.replace("./");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.transition-box {
+    margin-bottom: 10px;
+    width: 200px;
+    height: 100px;
+    border-radius: 4px;
+    background-color: #409EFF;
+    text-align: center;
+    color: #fff;
+    padding: 40px 20px;
+    box-sizing: border-box;
+    margin-right: 20px;
+    
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <!-- 操作页面 -->
+    <el-row>
+      <el-button type="primary" class="btn" @click="animation()">内置过渡动画</el-button>
+    </el-row>
+    <!-- <router-link to="/Animation">Animation</router-link> | -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    // 内置动画
+    animation(){
+      this.$router.replace('/animation');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
